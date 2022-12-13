c062e3faad1a1338cbee5590ce5cc3e8cc909da0
Dannilo Almeida <77454609+DanniloAlmeida@users.noreply.github.com>
2022-12-12 19:50:21 -0500

    commit

---
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,67 @@
+// função pom
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play(); 
+
+
+}
+document.querySelector('.tecla_pom').onclick=tocaSomPom;
+
+// função clap
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();
+    
+}
+document.querySelector('.tecla_clap').onclick=tocaSomClap;
+// função tim
+
+function tocaSomTim() {
+    document.querySelector('#som_tecla_tim').play();
+
+}
+document.querySelector('.tecla_tim').onclick=tocaSomTim;
+// função Puff
+
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+    
+}
+document.querySelector('.tecla_puff').onclick=tocaSomPuff;
+// função Splash
+
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+    
+}
+document.querySelector('.tecla_splash').onclick=tocaSomSplash;
+// função Toim 
+
+function tocaSomToim (){
+    document.querySelector('#som_tecla_toim').play();
+    
+}
+document.querySelector('.tecla_toim').onclick=tocaSomToim;
+// função Psh
+
+function tocaSomPsh (){
+    document.querySelector('#som_tecla_psh').play();
+    
+}
+document.querySelector('.tecla_psh').onclick=tocaSomPsh;
+// função Tic
+
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+   
+}
+document.querySelector('.tecla_tic').onclick=tocaSomTic;
+// função Tom 
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+    
+}
+document.querySelector('.tecla_tom').onclick=tocaSomTom;
+
+
+ 
